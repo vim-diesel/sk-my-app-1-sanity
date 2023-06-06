93e6bd932dfe6be9ad2b0651c3cfbb69f6cad2f1
Ian Clark <ian.clark@hotmail.com>
2023-06-06 10:11:45 -0500

    updated schema upgrade to pnpm

---
+++ schemas/workshop.ts
@@ -0,0 +1,48 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'workshop',
+  title: 'Workshop',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      validation: (Rule) => Rule.required(),
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'excerpt',
+      title: 'Excerpt',
+      type: 'text',
+      rows: 4,
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'text',
+    }),
+    defineField({
+        name: 'date',
+        title: 'Date',
+        type: 'datetime',
+      }),
+  ],
+})
